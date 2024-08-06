8831144e8cf498661c0e0adbfca046fb67955444
AjayPratapS1 <ajaypratapsingh8958a@gmail.com>
2024-08-07 00:25:55 +0530

    fix cors url

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://studynotion-frontend-kohl.vercel.app/",
+    origin: "https://studynotion-frontend-kohl.vercel.app",
     credentials: true,
   })
 );
